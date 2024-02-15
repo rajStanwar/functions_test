c3acf1e9b3aab5fa94ffbe264175807189be6b0e
Raj Singh <raj.tanwar@techdigitem.com>
2024-02-15 15:42:25 +0530

    updated monngo uri

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -4,18 +4,13 @@ import pyjokes
 import pymongo
 import os
 
-mongo_url = os.environ.get("DATABASE_URL")
-password = os.environ.get("DATABASE_PASSWORD")
-db_user = os.environ.get("DATABASE_USER")
-db_name = os.environ.get("DATABASE_USER")
+
 
 def main():
-  print(mongo_url)
-  print(password)
-  print(db_user)
-  print(db_name)
-  client = pymongo.MongoClient(f"mongodb+srv://{db_user}:{password}@{mongo_url}/{db_name}")
-  db = client[db_name]
+  mongo_uri = os.environ.get("MONGO_URI")
+  print(mongo_uri)
+  client = pymongo.MongoClient(mongo_uri)
+  db = client["myriad"]
   joke = pyjokes.get_joke()
   
   # insert joke in the database
