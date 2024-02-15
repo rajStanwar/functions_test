7b733f727dc3beaf433d460fbedc6827558e211e
Raj Singh <raj.tanwar@techdigitem.com>
2024-02-15 17:13:01 +0530

    updarted

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -7,7 +7,7 @@ import os
 
 
 def main():
-  mongo_uri = "mongodb+srv://doadmin:2Y7x5o01z4b3OpN6@mongodb-do-function-test-6c227d6b.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=mongodb-do-function-test"
+  mongo_uri = "mongodb+srv://doapps-e44dd091-4713-4513-9ea5-222647a16f3a:2Y7x5o01z4b3OpN6@mongodb-do-function-test-6c227d6b.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=mongodb-do-function-test"
   # print(mongo_uri)
   # client = pymongo.MongoClient(mongo_uri)
   # db = client["myriad"]
