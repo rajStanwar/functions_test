3c92d6de0e4cd727dda4b2dc8c90fcb2bd2c778e
Raj Singh <raj.tanwar@techdigitem.com>
2024-02-15 17:25:00 +0530

    message

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -7,20 +7,28 @@ import os
 
 
 def main():
-  mongo_uri = "mongodb+srv://doapps-e44dd091-4713-4513-9ea5-222647a16f3a:2Y7x5o01z4b3OpN6@mongodb-do-function-test-6c227d6b.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=mongodb-do-function-test"
-  print(mongo_uri)
-  client = pymongo.MongoClient(mongo_uri)
-  db = client["myriad"]
-  joke = pyjokes.get_joke()
-  
-  # insert joke in the database
-  print("Joke: ", joke)
-  
-  db.jokes.insert_one({"joke": joke})
-  return {
-    "body": {
-      "response_type": "in_channel",
-      "text": joke,
-      "uri": mongo_uri
+  try:
+    mongo_uri = "mongodb+srv://doapps-e44dd091-4713-4513-9ea5-222647a16f3a:9e2506TgD3YE4lW1@mongodb-do-function-test-6c227d6b.mongo.ondigitalocean.com/myriad"
+    print(mongo_uri)
+    client = pymongo.MongoClient(mongo_uri)
+    db = client["myriad"]
+    joke = pyjokes.get_joke()
+    
+    # insert joke in the database
+    print("Joke: ", joke)
+    
+    db.jokes.insert_one({"joke": joke})
+    return {
+      "body": {
+        "response_type": "in_channel",
+        "text": joke,
+        "uri": mongo_uri
+      }
     }
-  }
+  except Exception as e:
+    return {
+      "body": {
+        "response_type": "error",
+        "error": e,
+      }
+    }
