a07278ac6181b19ddfbabb9f025a3f31f28c7ea9
Raj Singh <raj.tanwar@techdigitem.com>
2024-02-15 17:15:13 +0530

    updated

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -8,15 +8,15 @@ import os
 
 def main():
   mongo_uri = "mongodb+srv://doapps-e44dd091-4713-4513-9ea5-222647a16f3a:2Y7x5o01z4b3OpN6@mongodb-do-function-test-6c227d6b.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=mongodb-do-function-test"
-  # print(mongo_uri)
-  # client = pymongo.MongoClient(mongo_uri)
-  # db = client["myriad"]
+  print(mongo_uri)
+  client = pymongo.MongoClient(mongo_uri)
+  db = client["myriad"]
   joke = pyjokes.get_joke()
   
   # insert joke in the database
   print("Joke: ", joke)
   
-  # db.jokes.insert_one({"joke": joke})
+  db.jokes.insert_one({"joke": joke})
   return {
     "body": {
       "response_type": "in_channel",
