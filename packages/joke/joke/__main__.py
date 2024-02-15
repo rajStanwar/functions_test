d7c4a9d398f0426861e3f0948bd2b5def8c7b853
Raj Singh <raj.tanwar@techdigitem.com>
2024-02-15 15:30:05 +0530

    removed mongo connection

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -1,30 +1,30 @@
 print("Inside main.py script")
 
 import pyjokes
-import pymongo
-import os
+# import pymongo
+# import os
 
-mongo_url = os.environ.get("DATABASE_URL")
-password = os.environ.get("DATABASE_PASSWORD")
-db_user = os.environ.get("DATABASE_USER")
-db_name = os.environ.get("DATABASE_USER")
+# mongo_url = os.environ.get("DATABASE_URL")
+# password = os.environ.get("DATABASE_PASSWORD")
+# db_user = os.environ.get("DATABASE_USER")
+# db_name = os.environ.get("DATABASE_USER")
 
 def main():
-  print(mongo_url)
-  print(password)
-  print(db_user)
-  print(db_name)
-  client = pymongo.MongoClient(f"mongodb+srv://{db_user}:{password}@{mongo_url}/{db_name}")
-  db = client[db_name]
+  # print(mongo_url)
+  # print(password)
+  # print(db_user)
+  # print(db_name)
+  # client = pymongo.MongoClient(f"mongodb+srv://{db_user}:{password}@{mongo_url}/{db_name}")
+  # db = client[db_name]
   joke = pyjokes.get_joke()
   
   # insert joke in the database
   print("Joke: ", joke)
   
-  db.jokes.insert_one({'joke': joke})
+  # db.jokes.insert_one({"joke": joke})
   return {
-    'body': {
-      'response_type': 'in_channel',
-      'text': joke
+    "body": {
+      "response_type": "in_channel",
+      "text": joke
     }
   }
