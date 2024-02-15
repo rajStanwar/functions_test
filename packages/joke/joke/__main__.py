a57d189e83fc80ae1621a22c564d7d1589da3c56
Raj Singh <raj.tanwar@techdigitem.com>
2024-02-15 15:51:22 +0530

    removed mongo

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -7,16 +7,16 @@ import os
 
 
 def main():
-  mongo_uri = os.environ.get("MONGO_URI")
-  print(mongo_uri)
-  client = pymongo.MongoClient(mongo_uri)
-  db = client["myriad"]
+  # mongo_uri = os.environ.get("MONGO_URI")
+  # print(mongo_uri)
+  # client = pymongo.MongoClient(mongo_uri)
+  # db = client["myriad"]
   joke = pyjokes.get_joke()
   
   # insert joke in the database
   print("Joke: ", joke)
   
-  db.jokes.insert_one({"joke": joke})
+  # db.jokes.insert_one({"joke": joke})
   return {
     "body": {
       "response_type": "in_channel",
