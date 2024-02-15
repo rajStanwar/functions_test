e0ac21f3ac274c17719b285924c4591382a41959
Raj Singh <raj.tanwar@techdigitem.com>
2024-02-15 13:31:32 +0530

    added print

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -8,11 +8,17 @@ db_user = os.environ.get("DATABASE_USER")
 db_name = os.environ.get("DATABASE_USER")
 
 def main():
+  print(mongo_url)
+  print(password)
+  print(db_user)
+  print(db_name)
   client = pymongo.MongoClient(f"mongodb+srv://{db_user}:{password}@{mongo_url}/{db_name}")
   db = client[db_name]
   joke = pyjokes.get_joke()
   
   # insert joke in the database
+  print("Joke: ", joke)
+  
   db.jokes.insert_one({'joke': joke})
   return {
     'body': {
