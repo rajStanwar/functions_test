1eda2eb0d1b30247756aea395e5dd7b5300a7d8a
Raj Singh <raj.tanwar@techdigitem.com>
2024-02-15 13:28:26 +0530

    removed args

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -7,7 +7,7 @@ password = os.environ.get("DATABASE_PASSWORD")
 db_user = os.environ.get("DATABASE_USER")
 db_name = os.environ.get("DATABASE_USER")
 
-def main(args):
+def main():
   client = pymongo.MongoClient(f"mongodb+srv://{db_user}:{password}@{mongo_url}/{db_name}")
   db = client[db_name]
   joke = pyjokes.get_joke()
