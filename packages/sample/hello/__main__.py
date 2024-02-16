3191041fed69bc489c5466fadbd563e2e76c28b2
Raj Singh <raj.tanwar@techdigitem.com>
2024-02-16 08:57:48 +0530

    env

--- packages/sample/hello/__main__.py
+++ packages/sample/hello/__main__.py
@@ -1,6 +1,12 @@
+import os
 def main(args):
       name = args.get("name", "stranger")
       greeting = "Hello " + name + "!"
       print(greeting)
-      return {"body": greeting}
+      return {
+            "body": {
+                  "message": greeting,
+                  "database": os.getenv('DATABASE_HOST')
+                  }
+      }
   
